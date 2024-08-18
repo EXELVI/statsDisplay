c0a6fd3e2e477c58aa04cab2f1e8efd147fc4108
EXELVI <exelvi@outlook.com>
2024-08-19 00:53:54 +0200

    feat: Add RSS news

--- startup.lua
+++ startup.lua
@@ -1,11 +1,12 @@
-local monitor = peripheral.wrap("right");
-local detector = peripheral.wrap("top");
+local monitor = peripheral.wrap("left");
+local detector = peripheral.wrap("environmentDetector_0");
 monitor.clear();
 local running = true;
 function getCurrentTime()
 	local time = os.time();
 	return textutils.formatTime(time, true);
 end;
+
 local function centerText(monitor, yPos, text, color)
 	local width, height = monitor.getSize();
 	local xPos = math.floor((width - string.len(text)) / 2) + 1;
@@ -36,53 +37,84 @@ function isNight()
 	local time = os.time();
 	return time >= 18;
 end;
-parallel.waitForAny(handleEvents, function()
-	while true do
-		local time = getCurrentTime();
-		monitor.setTextScale(2);
-		centerText(monitor, 4, "EXELVI's ", colors.blue);
-		if display == "time" then
-			centerText(monitor, 6, "               ", colors.white);
-			centerText(monitor, 6, time, colors.white);
-		elseif display == "weather" then
-			local rain = detector.isRaining();
-			local isThunder = detector.isThunder();
-			local randomValue = math.random(1, 5);
-			local isTrue = randomValue == 1;
-			local isNighttime = isNight();
-			local weather = rain and (isThunder and "Temporali" or "Pioggia") or (isNighttime and "Cielo pulito" or "Soleggiato");
-			centerText(monitor, 6, "               ", colors.white);
-			local txtcolor = colors.white;
-			if rain then
-				if isThunder then
-					txtcolor = isTrue and colors.yellow or colors.blue;
-				else
-					txtcolor = colors.blue;
-				end;
-			elseif isNighttime then
-				txtcolor = colors.lightBlue;
-			else
-				txtcolor = colors.yellow;
-			end;
-			centerText(monitor, 6, weather, txtcolor);
-		elseif display == "radiation" then
-			local radiation = detector.getRadiation();
-			local radiationRaw = detector.getRadiationRaw();
-			centerText(monitor, 6, "               ", colors.white);
-			local txtcolor = colors.white;
-			if radiationRaw < 0.0000002 then
-				txtcolor = colors.green;
-			elseif radiationRaw < 0.0005 then
-				txtcolor = colors.yellow;
-			elseif radiationRaw < 0.1 then
-				txtcolor = colors.orange;
-			elseif radiationRaw < 10 then
-				txtcolor = colors.red;
-			else
-				txtcolor = colors.purple;
-			end;
-			centerText(monitor, 6, math.floor(radiation.radiation * 100) / 100 .. " " .. radiation.unit, txtcolor);
-		end;
-		os.sleep(0.1);
+
+local rssFeed = "https://www.repubblica.it/rss/homepage/rss2.0.xml?ref=RHFT";
+local response = http.get(rssFeed);
+if response then
+	local rssData = response.readAll();
+	response.close();
+	local titoli = {};
+	for titolo in rssData:gmatch("<title>(.-)</title>") do
+		table.insert(titoli, titolo);
 	end;
-end);
+	print("Titoli caricati: " .. #titoli);
+	parallel.waitForAny(handleEvents, function()
+		local timee = 0;
+		while true do
+			local time = getCurrentTime();
+			monitor.setTextScale(2);
+			centerText(monitor, 4, "EXELVI's", colors.blue);
+			if display == "time" then
+				centerText(monitor, 6, "               ", colors.white);
+				centerText(monitor, 6, time, colors.white);
+			elseif display == "weather" then
+				local rain = detector.isRaining();
+				local isThunder = detector.isThunder();
+				local randomValue = math.random(1, 5);
+				local isTrue = randomValue == 1;
+				local isNighttime = isNight();
+				local weather = rain and (isThunder and "Temporali" or "Pioggia") or
+					(isNighttime and "Cielo pulito" or "Soleggiato");
+				centerText(monitor, 6, "               ", colors.white);
+				local txtcolor = colors.white;
+				if rain then
+					if isThunder then
+						txtcolor = isTrue and colors.yellow or colors.blue;
+					else
+						txtcolor = colors.blue;
+					end;
+				elseif isNighttime then
+					txtcolor = colors.lightBlue;
+				else
+					txtcolor = colors.yellow;
+				end;
+				centerText(monitor, 6, weather, txtcolor);
+			elseif display == "radiation" then
+				local radiation = detector.getRadiation();
+				local radiationRaw = detector.getRadiationRaw();
+				centerText(monitor, 6, "               ", colors.white);
+				local txtcolor = colors.white;
+				if radiationRaw < 0.0000002 then
+					txtcolor = colors.green;
+				elseif radiationRaw < 0.0005 then
+					txtcolor = colors.yellow;
+				elseif radiationRaw < 0.1 then
+					txtcolor = colors.orange;
+				elseif radiationRaw < 10 then
+					txtcolor = colors.red;
+				else
+					txtcolor = colors.purple;
+				end;
+				centerText(monitor, 6, math.floor(radiation.radiation * 100) / 100 .. " " .. radiation.unit, txtcolor);
+			end;
+
+
+
+			monitor.setCursorPos(1, 10);
+
+			if (timee > 10) then
+				timee = 0;
+				local titolo = titoli[math.random(1, #titoli)];
+				monitor.clearLine();
+				print(titolo);
+				monitor.write(titolo);
+			else 
+				timee = timee + 1;
+			end;
+
+			os.sleep(0.1);
+		end;
+	end);
+else
+	print("Errore nel caricamento del feed RSS");
+end;
